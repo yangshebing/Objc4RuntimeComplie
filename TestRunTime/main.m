696e1068ce9c5e3287da420d867594ea0e8e1787
yangshebing <shebing_yang@163.com>
2020-03-20 11:49:09 +0800

    first commit

---
+++ TestRunTime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestRunTime
+//
+//  Created by bg on 2020/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSArray *array = [[NSArray alloc] init];
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
